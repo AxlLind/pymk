0dda4cc1625dcc7b3da9f8791eecad7d4c3cd143
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-06-11 22:23:08 +0200

    Add variable expansion test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import pytest
 from typing import Generator
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from contextlib import redirect_stdout
+from contextlib import redirect_stdout, contextmanager
 
 import src as pymk
 from src import Target, PhonyTarget
@@ -13,6 +13,15 @@ from src import Target, PhonyTarget
 def tmpdir() -> Generator[Path, None, None]:
     with TemporaryDirectory() as tmpdir_str:
         yield Path(tmpdir_str)
+
+
+@contextmanager
+def pymk_variables(variables: dict[str, str]) -> Generator[None, None, None]:
+    pymk.set_variable(**variables)
+    try:
+        yield
+    finally:
+        pymk.module.VARIABLES.clear()
 
 
 def run_pymk(targets: list[PhonyTarget]) -> tuple[int, str]:
@@ -25,6 +34,38 @@ def test_trivial_command() -> None:
     status, output = run_pymk([PhonyTarget('x', cmd='echo hello world > /dev/null')])
     assert status == 0
     assert output.strip() == 'echo hello world > /dev/null'
+
+
+def test_var_expansion(tmpdir: Path) -> None:
+    with pymk_variables({'A': 'a', 'B': 'b', 'C': 'c', 'DEV_NULL': '/dev/null', 'ECHO': 'echo'}):
+        refers_to_output = Target(cmd='touch $OUTPUT', output=tmpdir / 'tmp.txt')
+        status, output = run_pymk(
+            [
+                PhonyTarget('0', cmd='echo $dependency > /dev/null', depends={'dependency': refers_to_output}),
+                PhonyTarget('1', cmd='echo $$VAR > /dev/null'),
+                PhonyTarget('2', cmd='echo $A$B$C > /dev/null'),
+                PhonyTarget('3', cmd='echo $(A)aa > /dev/null'),
+                PhonyTarget('4', cmd='echo $A/next/to/path > /dev/null'),
+                PhonyTarget('5', cmd='echo expansion at the end > $DEV_NULL'),
+                PhonyTarget('6', cmd='$ECHO > $DEV_NULL'),
+            ]
+        )
+        assert status == 0
+        assert f'touch {tmpdir / "tmp.txt"}' in output
+        assert 'echo $VAR > /dev/null' in output
+        assert 'echo abc > /dev/null' in output
+        assert 'echo aaa > /dev/null' in output
+        assert 'echo a/next/to/path > /dev/null' in output
+        assert 'echo expansion at the end > /dev/null' in output
+        assert 'echo > /dev/null' in output
+
+    status, output = run_pymk([PhonyTarget('a', cmd='echo $UNSET_VAR > /dev/null')])
+    assert status != 0
+    assert '$UNSET_VAR' in output
+
+    status, output = run_pymk([PhonyTarget('a', cmd='echo $OUTPUT > /dev/null')])
+    assert status != 0
+    assert '$OUTPUT' in output
 
 
 def test_simple_dependencies(tmpdir: Path) -> None:
