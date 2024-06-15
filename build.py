6146b4fc1bb359af3cdddba13e0ba713102e9881
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-06-15 08:38:56 +0200

    Dogfood pymk as a simple command runner

---
+++ build.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import pymk
+from pymk import PhonyTarget
+from pathlib import Path
+from os import chdir
+
+chdir(Path(__file__).parent)
+
+files = sorted([Path('build.py'), *Path('src').glob('*.py'), *Path('tests').glob('*.py')])
+pymk.set_variable(FILES=' '.join(str(f) for f in files))
+
+# fmt: off
+pymk.main([
+    PhonyTarget('mypy',   help='Type checking',    cmd='python3 -m mypy $FILES'),
+    PhonyTarget('lint',   help='Lint all files',   cmd='python3 -m ruff check $FILES'),
+    PhonyTarget('fmt',    help='Format all files', cmd='python3 -m ruff format $FILES'),
+    PhonyTarget('fmt-ok', help='Check formatting', cmd='python3 -m ruff format --check $FILES'),
+    PhonyTarget('test',   help='Run all tests',    cmd='python3 -m pytest -v tests'),
+])
